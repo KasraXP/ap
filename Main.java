2f4c406e9573591bea29b962ad0100ec058746e4
KasraXP <kasraupdown.65@gmail.com>
2025-03-02 18:56:24 +0330

    Main.java

--- Main.java
+++ Main.java
@@ -1,28 +1,10 @@
+public class Main {
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        float x;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter number: ");
-        x = input.nextFloat();
-
-
-        if (x == 0)
-            System.out.println("Zero");
-       else if ((x < 1) && (x > -1))
-            System.out.println(" Small");
-       else if (x > 1000000)
-            System.out.println("Large");
-       else if (x > 0)
-            System.out.println("Positive");
-       else if (x < 0)
-            System.out.println("Negative");
-
-
-
+        System.out.println("Hello World!");
 
     }
-}
+  }
