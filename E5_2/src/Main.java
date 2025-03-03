0986c0c1ad0f52ebc62e425a90fdf7e51dd8f921
VICTUS 16 <kasraupdown.65@gmail.com>
2025-03-03 18:36:41 +0330

    exercise 5.2

---
+++ E5_2/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void Main (String[] args) {
+
+        float x;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        x = input.nextFloat();
+
+
+        if (x == 0)
+            System.out.println("Zero");
+        else if ((x < 1) && (x > -1))
+            System.out.println(" Small");
+        else if ((x > 1000000) || (x < -1000000))
+            System.out.println("Large");
+        else if (x > 0)
+            System.out.println("Positive");
+        else if (x < 0)
+            System.out.println("Negative");
+
+
+
+
+    }
+}
