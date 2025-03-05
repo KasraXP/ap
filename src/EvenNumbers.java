634ad1fc8374b3a8759f35cad0b5dc3051ffc0d6
KasraXP <lkasraupdown.65@gmail.com>
2025-03-06 00:16:14 +0330

    E6.1(a)\

---
+++ src/EvenNumbers.java
@@ -0,0 +1,12 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+
+        int i, sum = 0;
+        for (i = 2; i <= 100 ;i += 2)
+            sum = sum + i;
+
+
+        System.out.println(sum);
+    }
+}
+
