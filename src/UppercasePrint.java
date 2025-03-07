f367f2c26e3f2a470dc961aec828e9c24723e71b
KasraXP <lkasraupdown.65@gmail.com>
2025-03-07 10:20:41 +0330

    E6.3(a)

---
+++ src/UppercasePrint.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class UppercasePrint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        System.out.print("Uppercase letters: ");
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            int ascii = (int) ch;
+
+            if (ascii >= 65 && ascii <= 90) {
+                System.out.print(ch);
+            }
+        }
+
+        System.out.println();
+        scanner.close();
+    }
+}
