b2da36ac90eb95303b06170665c8302965caca90
KasraXP <lkasraupdown.65@gmail.com>
2025-03-25 13:01:48 +0330

    EX2_PM_1_3

--- ap/exercises/ex2/Main_EX2_PM_1_3.java
+++ ap/exercises/ex2/Main_EX2_PM_1_3.java
@@ -1,23 +1,56 @@
 package exercises.ex2;
 
-import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
+
 public class Main_EX2_PM_1_3 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter any number for your hollow square: ");
+        System.out.println("Enter any number for your hollow square ");
         int k = sc.nextInt();
-        System.out.println("Enter the number of dots :");
+        System.out.println("Enter the number of dots ");
         int c = sc.nextInt();
-        String[][] square = new String[k][k];
+
+        if (c > k * k) {
+            while (c > k * k) {
+                System.out.println("Too many dots!");
+                System.out.println("Please enter a smaller number ");
+                c = sc.nextInt();
+            }
+
+        }
+
+        char[][] square = new char[k + 2][k + 2];
 
         for (int i = 0; i < k + 2; i++) {
             for (int j = 0; j < k + 2; j++) {
                 if (i == 0 || i == k + 1 || j == 0 || j == k + 1)
-                    System.out.print("*");
+                    square[i][j] = '*';
                 else
-                    System.out.print(".");
+                    square[i][j] = ' ';
+            }
+
+        }
+
+        Random rand = new Random();
+        int PlacedDots = 0;
+
+        while (PlacedDots < c) {
+            int x = rand.nextInt(k - 1) + 1;
+            int y = rand.nextInt(k - 1) + 1;
+
+            if (square[x][y] == ' ') {
+                square[x][y] = '.';
+                PlacedDots++;
+            }
+
+        }
+
+        for (int i = 0; i < k + 2; i++) {
+            for (int j = 0; j < k + 2; j++) {
+                System.out.print(square[i][j]);
+
             }
             System.out.println();
         }
